11bf506ace36566d75f6034e019950dd3753518d
stephaniebeatrice0 <stephanie.beatrice@softclans.co.ke>
2024-03-29 18:30:27 +0300

    first commit

---
+++ app/config/assets/commonDefinitions.js
@@ -0,0 +1,56 @@
+window.odkCommonDefinitions = {
+  "_tokens": {},
+  "_locales": {
+    "setting_name": "_locales",
+    "_row_num": 5,
+    "value": [
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "en": "English",
+              "hi": "अंग्रेज़ी",
+              "es": "Inglés",
+              "default": "English"
+            }
+          }
+        },
+        "_row_num": 7,
+        "name": "en"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "en": "Hindi",
+              "hi": "हिंदी",
+              "es": "Hindi",
+              "default": "Hindi"
+            }
+          }
+        },
+        "_row_num": 8,
+        "name": "hi"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "en": "Spanish",
+              "hi": "स्पेनिश",
+              "es": "Español",
+              "default": "Spanish"
+            }
+          }
+        },
+        "_row_num": 9,
+        "name": "es"
+      }
+    ]
+  },
+  "_default_locale": {
+    "setting_name": "_default_locale",
+    "_row_num": 5,
+    "value": "en"
+  }
+}
